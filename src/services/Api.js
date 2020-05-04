0d5f77a617bb2464c14044977fa91d1186a30921
dylanbr0wn <dylan.bro97@gmail.com>
2020-05-03 19:03:42 -0700

    initial

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: 'http://localhost:8000/'
+        baseURL: process.env.API_URL ? process.env.API_URL : 'http://localhost:8000/'
 
     })
 }
