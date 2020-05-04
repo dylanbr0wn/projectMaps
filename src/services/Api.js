df7603987d1f271c4d3ea7748a48552468c85b8b
dylanbr0wn <dylan.bro97@gmail.com>
2020-05-03 19:07:33 -0700

    fix api target

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: process.env.API_URL ? process.env.API_URL : 'http://localhost:8000/'
+        baseURL: (process.env._ && process.env._.indexOf("heroku")) ? process.env.API_URL : 'http://localhost:8000/'
 
     })
 }
