c7b304506d46094449c12bce4e747eb9f306988c
dylanbr0wn <dylan.bro97@gmail.com>
2020-05-03 19:21:24 -0700

    fix api target

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8000/' : process.env.API_URL,
+        baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:8000/' : process.env.REACT_APP_API_URL,
 
     })
 }
